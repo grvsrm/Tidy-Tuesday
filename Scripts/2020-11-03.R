8c242272bf8385e67b9e7a1dba47ecec11b80204
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-04 15:04:49 +0530

    updated script

--- Scripts/2020-11-03.R
+++ Scripts/2020-11-03.R
@@ -20,7 +20,8 @@ ikea <- read_rds("Data/ikea_raw.rds")
 
 ikea_cleaned <- ikea %>% 
     select(-X1) %>% 
-    mutate(category = str_to_title(category))
+    mutate(category = str_to_title(category),
+           price_usd = 0.27 * price)
 
 ### Tidy Exploration starts here..
 
@@ -33,22 +34,43 @@ ikea_cleaned %>%
          x = "",
          y= "")
 
-ikea_cleaned %>% 
-    add_count(category, name = "category_total") %>% 
-    mutate(category = glue("{category} ({category_total})"),
-           category = fct_reorder(category, price)) %>% 
-    ggplot(aes(price, category, color = category)) +
-    geom_boxplot(show.legend = F) +
-    geom_jitter(width = 0, height = 0.2, alpha = 0.4, show.legend = F) +
-    theme_classic() +
-    scale_x_log10()
 
+##################################################################
 
 
 ikea_cleaned %>%
-    ggplot(aes(price, fill = category)) +
-    geom_histogram() +
-    facet_wrap( ~ category, scales = "free") +
-    theme(legend.position = "none") +
-    scale_x_log10()
+    add_count(category, name = "category_total") %>%
+    mutate(
+        category = glue("{category} ({category_total})"),
+        category = fct_reorder(category, price_usd)
+    ) %>%
+    ggplot(aes(price_usd, category, color = category)) +
+    geom_boxplot(show.legend = F) +
+    geom_jitter(
+        width = 0,
+        height = 0.2,
+        alpha = 0.4,
+        show.legend = F
+    ) +
+    theme_classic() +
+    scale_x_log10(label = dollar_format()) +
+    labs(title = "How much do items in each category cost ?",
+         x = "Price(USD)",
+         y = "")
 
+
+###################################################################
+
+
+ikea_cleaned %>%
+    ggplot(aes(price_usd, fill = category)) +
+    geom_histogram() +
+    facet_wrap(~ category, scales = "free") +
+    theme(legend.position = "none") +
+    scale_x_log10(labels = dollar_format()) +
+    labs(title = "How much do items in each category cost ?",
+         x = "Price(USD)",
+         y = "")
+
+
+###################################################################
