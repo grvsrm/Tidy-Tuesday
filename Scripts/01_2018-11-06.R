a8d68e8d3f94cbde3c2f30daae61a78e5d0c85ad
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-05 14:19:34 +0530

    created a new script

---
+++ Scripts/01_2018-11-06.R
@@ -0,0 +1,12 @@
+# Load the libraries
+
+
+
+
+# Download and save the data as an rds object
+
+ttfile <- tidytuesdayR::tt_load("2018-11-06")
+
+ttfile$us_wind %>% 
+    write_rds(here("data", "us_wind_raw.rds"))
+
