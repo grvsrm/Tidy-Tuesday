1081f75977fabe2d0466ffb27a3757a3ba34781e
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-05 14:12:07 +0530

    updated lm model

--- Scripts/02_2020-11-03.R
+++ Scripts/02_2020-11-03.R
@@ -9,5 +9,12 @@ ikea_cleaned <- read_rds(here("data" ,"ikea_cleaned.rds"))
 
 ikea_cleaned %>% 
     select(item_id, category, price_usd, sellable_online, other_colors, designer, category_total) %>% 
-    nest(-category) %>% 
-    map()
+    nest(data = c(item_id, price_usd, sellable_online, other_colors, designer, 
+                  category_total)) %>% 
+    mutate(model = map(data, ~lm(price_usd~sellable_online + other_colors + designer + category_total, data = .)),
+           tidied = map(model, tidy)) %>% 
+    unnest(tidied)
+
+
+ 
+
