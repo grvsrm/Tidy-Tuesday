81d036c1a43ed78dac415f0d6a1d884318a2c623
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-05 12:41:22 +0530

    updated script

--- Scripts/02_2020-11-03.R
+++ Scripts/02_2020-11-03.R
@@ -7,3 +7,7 @@ library(here)
 
 ikea_cleaned <- read_rds(here("data" ,"ikea_cleaned.rds"))
 
+ikea_cleaned %>% 
+    select(item_id, category, price_usd, sellable_online, other_colors, designer, category_total) %>% 
+    nest(-category) %>% 
+    map()
