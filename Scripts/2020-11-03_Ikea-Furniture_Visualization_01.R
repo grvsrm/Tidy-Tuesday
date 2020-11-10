6dd2df74ec5058f257247966561b30ff0e4ddf02
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-10 16:18:59 +0530

    updated script

--- Scripts/2020-11-03_Ikea-Furniture_Visualization_01.R
+++ Scripts/2020-11-03_Ikea-Furniture_Visualization_01.R
@@ -81,8 +81,8 @@ lines <- stem_labels %>%
 
 plot <- ggraph(final_graph, layout = 'dendrogram', circular = TRUE) + 
     geom_edge_diagonal(colour  = "#FFDA1A", alpha = 0.1, width = 0.9) +
-    geom_node_text(aes(x = x*3, y = y*3, label = glue("{label}\n({scales::percent(percent, accuracy = 0.1)})"), filter = leaf == FALSE & node != 'root', hjust = ifelse(between(node_angle(x,y), 90, 270), 1, 0)), size = 3, color = "white", family = "Noto Sans Bold", data = stem_labels) +
-    geom_segment(aes(x = 2*xold, xend = 2.8*x, y = 2*y, yend = 2.8*yend), data = lines, color = "white", size = 0.2) +
+    geom_node_text(aes(x = x*3, y = y*3, label = glue("{label}\n({scales::percent(percent, accuracy = 0.1)})"), filter = leaf == FALSE & node != 'root', hjust = ifelse(between(node_angle(x,y), 90, 270), 1, 0)), size = 3, color = "blue", family = "Noto Sans Bold", data = stem_labels) +
+    geom_segment(aes(x = 2*xold, xend = 2.8*x, y = 2*y, yend = 2.8*yend), data = lines, color = "blue", size = 0.2) +
     geom_node_point(aes(filter = leaf), colour  = "#FFDA1A") +
     annotate("label", x = 0, y = 0.15, label = "IKEA", family = "Anton", colour  = "#FFDA1A", size = 20, label.size = 0, fill = "#0051ba") +
     annotate("label", x = 0, y = -0.085, label = "Catalogue", family = "Anton", colour  = "#FFDA1A", size = 8, label.size = 0, fill = "#0051ba") +
@@ -92,9 +92,9 @@ plot <- ggraph(final_graph, layout = 'dendrogram', circular = TRUE) +
          title = NULL,
          subtitle = NULL,
          caption = "**Data**: IKEA & Kaggle | **Graphic**: @jakekaupp") +
-    theme_jk(dark = TRUE, 
-             grid = FALSE,
-             markdown = TRUE) +
+#    theme_jk(dark = TRUE, 
+#             grid = FALSE,
+#             markdown = TRUE) +
     theme(legend.position= "none",
           plot.background = element_rect(fill = "#0051ba"),
           axis.text.x = element_blank(),
@@ -102,8 +102,8 @@ plot <- ggraph(final_graph, layout = 'dendrogram', circular = TRUE) +
     expand_limits(x = c(-2, 2), y = c(-2, 2)) +
     coord_equal() 
 
-ggsave(here("2020", "week45", "tw45_plot.png"), plot, width = 12, height = 12)
+ggsave(here("Plots", "ikea_plot.png"), plot, width = 12, height = 12)
 
-image_read(here("2020", "week45", "tw45_plot.png")) %>% 
+image_read(here("Plots", "ikea_plot.png")) %>% 
     image_trim() %>% 
-    image_write(here("2020", "week45", "tw45_plot.png"))
+    image_write(here("Plots", "ikea_plot.png"))
