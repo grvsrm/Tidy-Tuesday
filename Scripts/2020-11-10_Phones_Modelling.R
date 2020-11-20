3c5a35550f1324631f965d8835d8b781ed590cd5
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-20 21:06:26 +0530

    updated script

--- Scripts/2020-11-10_Phones_Modelling.R
+++ Scripts/2020-11-10_Phones_Modelling.R
@@ -1,6 +1,7 @@
 # Prerequisites
 library(tidyverse)
-library(fpp3)
+library(here)
+library(directlabels)
 
 
 # Load the data
@@ -20,12 +21,48 @@ phones %>%
          x = "",
          y = "Subscriptions per 100 people")
 
-phones %>% 
-    ggplot(aes(year, subscriptions, color = type, group = interaction(type, country))) +
-    geom_line()
 
 country_sizes <- phones %>% 
     group_by(country) %>% 
     summarise(avg_population = mean(total_pop, na.rm = T))
 
-    
+phones %>% 
+    semi_join(country_sizes %>% 
+                  top_n(40, avg_population)) %>% 
+    filter(!is.na(subscriptions)) %>% 
+    ggplot(aes(year, subscriptions, color = type, group = interaction(type, country))) +
+    geom_line() +
+    geom_dl(aes(label = country), method = "last.points") +
+    facet_wrap(~continent) 
+
+
+phones %>% 
+    group_by(year, continent) %>% 
+    summarise(subs = mean(subscriptions, na.rm = T)) %>% 
+    ggplot(aes(year, subs, color = continent)) +
+    geom_line() +
+    geom_dl(aes(label = continent), method = "last.points") +
+    theme(legend.position = "none")
+
+phones %>% 
+    semi_join(country_sizes %>% 
+                  top_n(40, avg_population)) %>% 
+    filter(!is.na(income)) %>% 
+    ggplot(aes(year, subscriptions, color = type, group = interaction(type, country))) +
+    geom_line() +
+    geom_dl(aes(label = country), method = "last.points") +
+    facet_wrap(~income) 
+
+
+summarise_subscriptions = . %>% 
+    filter(!is.na(subscriptions)) %>% 
+    summarise(avg_subscriptions = mean(subscriptions, na.rm = T),
+              median_subscriptions = median(subscriptions),
+              p25 = quantile(subscriptions, .25),
+              p75 = quantile(subscriptions, .75))
+
+phones %>% 
+    group_by(country) %>% 
+    summarise_subscriptions()
+
+
