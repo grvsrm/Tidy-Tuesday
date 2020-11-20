28027051e416bf839557efd02a702525af171f74
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-20 16:02:09 +0530

    updated script

--- Scripts/2020-11-10_Phones_Modelling.R
+++ Scripts/2020-11-10_Phones_Modelling.R
@@ -1,6 +1,6 @@
 # Prerequisites
 library(tidyverse)
-
+library(fpp3)
 
 
 # Load the data
@@ -13,7 +13,19 @@ phones %>%
            !is.na(subscriptions)) %>% 
     ggplot(aes(year, subscriptions, color = type)) +
     geom_line() +
+    scale_x_continuous(breaks = 1:30,
+                       labels = seq(1990, 2019, 1)) +
     labs(title = "Phone Subscriptions over years",
          color = "",
          x = "",
          y = "Subscriptions per 100 people")
+
+phones %>% 
+    ggplot(aes(year, subscriptions, color = type, group = interaction(type, country))) +
+    geom_line()
+
+country_sizes <- phones %>% 
+    group_by(country) %>% 
+    summarise(avg_population = mean(total_pop, na.rm = T))
+
+    
