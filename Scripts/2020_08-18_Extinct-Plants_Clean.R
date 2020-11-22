ed067c89524a09f10f1c61574f6e5dbbcb5c680d
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-22 14:31:49 +0530

    created cleaning script

---
+++ Scripts/2020_08-18_Extinct-Plants_Clean.R
@@ -0,0 +1,33 @@
+# Prerequisites
+library(tidyverse)
+library(here)
+library(janitor)
+
+# Download and save the data
+tt <- tidytuesdayR::tt_load("2020-08-18")
+
+
+# Save
+tt$plants %>% 
+    write_rds(here("data", "plants_raw.rds"))
+
+tt$threats %>% 
+    write_rds(here("data", "threats_raw.rds"))
+
+tt$actions %>% 
+    write_rds(here("data", "actions_raw.rds"))
+
+
+plants <- read_rds(here("data", "plants_raw.rds")) %>% 
+    clean_names() %>% 
+    remove_empty(c("rows", "cols"))
+    
+threats <- read_rds(here("data", "threats_raw.rds")) %>% 
+    clean_names() %>% 
+    remove_empty(c("rows", "cols"))
+
+actions <- read_rds(here("data", "actions_raw.rds")) %>% 
+    clean_names() %>% 
+    remove_empty(c("rows", "cols"))
+
+
