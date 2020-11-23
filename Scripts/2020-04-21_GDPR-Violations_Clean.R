231a3ef36f1a45038c715e128627b9ff9dc6bbc3
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-23 16:29:15 +0530

    updated script

--- Scripts/2020-04-21_GDPR-Violations_Clean.R
+++ Scripts/2020-04-21_GDPR-Violations_Clean.R
@@ -7,16 +7,24 @@ library(lubridate)
 # Get the Data
 
 read_tsv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-21/gdpr_violations.tsv') %>% 
-    write_rds(here("data", "gdpr_violations.rds"))
+    write_rds(here("data", "gdpr_violations_raw.rds"))
 
 read_tsv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-21/gdpr_text.tsv') %>% 
-    write_rds(here("data", "gdpr_text.rds"))
+    write_rds(here("data", "gdpr_text_raw.rds"))
 
 
 # Clean the data
-gdpr_violation <- read_rds(here("data", "gdpr_violations.rds")) %>% 
-    mutate(date = mdy(date))
+gdpr_violation <- read_rds(here("data", "gdpr_violations_raw.rds")) %>% 
+    mutate(date = mdy(date),
+           date = na_if(date, "1970-01-01")) %>% 
+    rename("country" = name)
 
-gdpr_violation %>% view()
 
 
+
+
+
+# Save the data
+
+gdpr_violation %>% 
+    write_rds(here("data", "gdpr_violations.rds"))
