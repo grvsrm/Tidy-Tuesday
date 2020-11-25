776e6c3c1fd9ba89e7133ccdf9f3a53b1e6461ca
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-25 16:37:16 +0530

    updated script with rf model

--- Scripts/2020-02-18_Food-Consumption_Model.R
+++ Scripts/2020-02-18_Food-Consumption_Model.R
@@ -20,7 +20,58 @@ food <- food_consumption %>%
     clean_names() %>% 
     mutate_if(is_character, factor)
 
-    
+# Explore
+food %>% 
+    GGally::ggpairs(aes(color = asia))
+
+# Modeling
+
+# Resamples
+
+food_boot <- food %>% 
+    bootstraps(strata = asia)
+
+# GLM Model Spec
+glm_spec <- logistic_reg() %>% 
+    set_engine("glm")
+
+# Fit
+
+glm_fit <- fit_resamples(glm_spec,
+              asia~.,
+              resamples = food_boot,
+              control = control_resamples(save_pred = T, verbose = T),
+              metrics = metric_set(accuracy, sensitivity, specificity, roc_auc))
+
+glm_fit %>% 
+    collect_metrics()
 
 
 
+# RF Model Spec with tuning parameters
+
+rf_spec <- rand_forest(mtry = tune(),
+            trees = 1000,
+            min_n = tune()) %>% 
+    set_mode("classification") %>% 
+    set_engine("ranger")
+
+
+# Fit 
+
+rf_fit <- tune_grid(rf_spec,
+          asia~.,
+          resamples = food_boot,
+          control = control_resamples(save_pred = T, verbose = T),
+          metrics = metric_set(accuracy, sensitivity, specificity, roc_auc))
+
+rf_fit %>% 
+    collect_metrics()
+glm_fit %>% 
+    collect_metrics()
+
+rf_fit %>% 
+    show_best("roc_auc")
+
+
+# End of Sript
