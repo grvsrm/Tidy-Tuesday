e3872f82439c51a0a9f77d6c725f35e35a0a2c4e
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-25 09:32:29 +0530

    updated script

--- Scripts/2020-10-27_Canadian-Turbines_Clean.R
+++ Scripts/2020-10-27_Canadian-Turbines_Clean.R
@@ -7,20 +7,24 @@ library(here)
 # Download and save the data as an rds object
 
 readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-10-27/wind-turbine.csv') %>% 
-    write_rds(here("data", "wind_turbine.rds"))
+    write_rds(here("data", "wind_turbine_raw.rds"))
 
 
 # Cleaning
-wind_turbine_raw <- read_rds(here("data", "wind_turbine.rds"))
 
-wind_turbine <-  wind_turbine_raw %>% 
-    mutate(turbine_capacity = row_number(),
+wind_turbine <-  read_rds(here("data", "wind_turbine_raw.rds")) %>% 
+    transmute(turbine_capacity = turbine_rated_capacity_k_w,
            commissioning_date = parse_number(commissioning_date),
            model = fct_lump(model, 10),
-           province_territory = fct_lump(province_territory, 8)) %>% 
-    filter(!is.na(turbine_rated_capacity_k_w)) %>% 
-    select(turbine_capacity, commissioning_date, model, province_territory, rotor_diameter_m,
-           hub_height_m, turbine_rated_capacity_k_w) %>% 
+           province_territory = fct_lump(province_territory, 8),
+           rotor_diameter_m,
+           hub_height_m) %>% 
+    filter(!is.na(turbine_capacity)) %>% 
     mutate_if(is.character, factor)
 
+wind_turbine %>% 
+    write_rds(here("data", "wind_turbine.rds"))
 
+
+
+# End of Script
