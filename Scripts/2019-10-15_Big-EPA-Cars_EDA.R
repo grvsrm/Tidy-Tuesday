a04e348943419a203f558e53215dc42d71e6560d
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-27 19:37:49 +0530

    updated script

--- Scripts/2019-10-15_Big-EPA-Cars_EDA.R
+++ Scripts/2019-10-15_Big-EPA-Cars_EDA.R
@@ -2,8 +2,10 @@
 library(tidyverse)
 library(here)
 library(janitor)
+library(scales)
+library(broom)
 
-
+theme_set(theme_light())
 
 # load the data
 
@@ -22,6 +24,58 @@ big_epa_cars %>%
          y = "City MPG") +
     theme(legend.position = "none") +
     expand_limits(x = 0, y = 0)
+
+
+big_epa_cars %>% 
+    filter(cityE == 0) %>% 
+    mutate(VClass = fct_lump(VClass, 8),
+           VClass = fct_reorder(VClass, city08)) %>% 
+    ggplot(aes(city08, VClass)) +
+    geom_boxplot()
+
+
+big_epa_cars %>% 
+    filter(cityE == 0, !is.na(drive)) %>% 
+    mutate(drive = fct_lump(drive, 8),
+           drive = fct_reorder(drive, city08)) %>% 
+    ggplot(aes(city08, drive)) +
+    geom_boxplot()
+
+big_epa_cars %>% 
+    filter(cityE == 0) %>% 
+    ggplot(aes(city08, cylinders, group = cylinders)) +
+    geom_boxplot()
+
+big_epa_cars %>% 
+    filter(cityE == 0) %>% 
+    ggplot(aes(displ, city08)) +
+    geom_point(size =2) +
+    expand_limits(y = 0) +
+    geom_smooth(method = "lm")
+
+# Model ----
+
+# Goal is to predict fuel efficiency for non-electric cars
+
+non_electric_cars <- big_epa_cars %>% 
+    filter(cityA08 == 0, # Cars with single fuel source
+           cityE == 0) # Only Non Electric Cars 
+
+
+# A simple linear model
+lm(city08~displ, data = non_electric_cars) %>% 
+    augment() %>% 
+    names()
+    ggplot(aes(displ, city08)) +
+    geom_point() +
+    geom_line(aes(y = .fitted), size = 2, color = "red")
+
+
+# Linear model with splines
+library(splines)
+lm(city08 ~ ns(displ, 2), data = non_electric_cars) %>% 
+    summary()
+
 
 
 
