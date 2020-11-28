bf64b958e5bfa8fda9e420123ce02496ff468286
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-11-28 20:42:39 +0530

    updated script

--- Scripts/2019-10-15_Big-EPA-Cars_Clean.R
+++ Scripts/2019-10-15_Big-EPA-Cars_Clean.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(here)
 library(janitor)
-
+library(lubridate)
 
 # Download the data
 
@@ -14,13 +14,24 @@ library(janitor)
 # Clean the data
 
 big_epa_cars_cleaned <- read_rds(here("data", "big_epa_cars_raw.rds")) %>% 
-    mutate(uses_electricity = ifelse(highwayE>0, "uses_electricity", "doesn't use electricity"))
+    mutate(uses_electricity = ifelse(highwayE>0, "uses_electricity", "doesn't use electricity")) %>% 
+    clean_names() %>% 
+    remove_empty(c("rows", "cols")) %>% 
+    mutate(charger = paste(t_charger, s_charger),
+           charger = case_when(charger == "TRUE S" ~ "TS",
+                               charger == "NA S" ~ "S",
+                               charger == "TRUE NA" ~ "T",
+                               TRUE ~ "No Charger")) %>% 
+    select(-t_charger, -s_charger) %>% 
+    mutate_if(is.character, factor)
 
 
-big_epa_cars_cleaned %>% 
-    count(fuelType)
+big_epa_cars_cleaned
 
 # Save the clean data
 
 big_epa_cars_cleaned %>% 
     write_rds(here("data", "big_epa_cars.rds"))
+
+
+# End of Script
