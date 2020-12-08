ef611ca714f65996a2dcddfbc66989102ffd8a12
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-12-08 16:16:11 +0530

    created a visualization script

---
+++ Scripts/2020-11-24_Washington-Trails_Visualization_02.R
@@ -0,0 +1,14 @@
+# Pre requisites
+library(tidyverse)
+library(here)
+library(ggtext)
+library(showtext)
+library(ragg)
+
+
+# Font
+showtext_opts(dpi = 180)
+showtext_auto(enable = TRUE)
+
+
+# Data
