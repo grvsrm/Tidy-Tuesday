2668fc26e1223a5082456c554e41729ab7940d01
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2020-12-16 10:25:23 +0530

    created script

---
+++ Scripts/2020-12-15_Ninja-Warrior_Clean.R
@@ -0,0 +1,23 @@
+# Pre requisites
+library(tidyverse)
+library(here)
+library(janitor)
+
+
+# Data
+read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-12-15/ninja_warrior.csv') %>% 
+    write_rds(here("data", "ninja warrior_raw.rds"))
+
+
+
+# Clean the data
+
+ninja_cleaned <- read_rds(here("data", "ninja warrior_raw.rds")) %>% 
+    clean_names() %>% 
+    remove_empty(c("rows", "cols"))
+
+ninja_cleaned %>% view()
+
+
+ninja_cleaned %>% 
+    write_rds(here("data", "ninja_warrior_cleaned.rds"))
