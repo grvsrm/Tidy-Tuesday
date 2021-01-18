20d765de99d0348568f2c0faed065e379f9c960f
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2021-01-18 14:51:00 +0530

    updated script

--- Scripts/2021-01-05_Transit-Cost_Clean.R
+++ Scripts/2021-01-05_Transit-Cost_Clean.R
@@ -3,24 +3,28 @@ library(readr)
 library(janitor)
 library(here)
 library(dplyr)
-
+library(lubridate)
 
 # Download the data
 read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-05/transit_cost.csv') %>% 
     write_rds(here("data", "transit-cost-raw.rds"))
 
 
-# Read and clean the data
+# Read and clean and save the data
 read_rds(here("data", "transit-cost-raw.rds")) %>% 
     clean_names() %>% 
     remove_empty(c("rows", "cols")) %>% 
     filter(!is.na(e)) %>%
     rename("ID" = e,
            "iso2c" = country) %>%
-    mutate(country_name = countrycode::countrycode(iso2c, "iso2c", "iso.name.en"),
+    mutate(country_name = countrycode::countrycode(iso2c, "iso2c", "country.name"),
            country_name = case_when(is.na(country_name) ~ "United Kingdom",
-                                    TRUE ~ country_name)) %>% 
+                                    TRUE ~ country_name),
+           continent = countrycode::countrycode(country_name, "country.name", "continent"),
+           tunnel_per = tunnel/length) %>%
+    mutate_at(vars(start_year, end_year, real_cost), as.numeric) %>% 
     write_rds(here("data", "transit-cost-clean.rds"))
+
 
 
 
---
+++ Scripts/2021-01-05_Transit-Cost_EDA.R
@@ -0,0 +1,53 @@
+# Pre requisites
+library(tidyverse)
+library(here)
+library(scales)
+theme_set(theme_light())
+
+
+# Read the data
+transit_cost <- read_rds(here("data", "transit-cost-clean.rds"))
+
+
+
+# Explore
+transit_cost %>% 
+    filter(country_name == "India") %>% 
+    mutate(line = fct_reorder(line, year)) %>% 
+    ggplot(aes(xmin = start_year, xmax = end_year, y = line)) +
+    geom_errorbarh(aes(color = city)) +
+    labs(x = "Year",
+         y = "",
+         color = "City")
+
+transit_cost %>% 
+    ggplot(aes(cost_km_millions)) +
+    geom_histogram()
+
+transit_cost %>% 
+    filter(!is.na(cost_km_millions),
+           tunnel_per == 1) %>% 
+    mutate(country_name = fct_lump(country_name, 12)) %>% 
+    add_count(country_name) %>% 
+    mutate(country_name = glue::glue("{country_name} ({n})"),
+           country_name = fct_reorder(country_name, cost_km_millions)) %>% 
+    ggplot(aes(cost_km_millions, country_name)) +
+    geom_boxplot() +
+    scale_x_continuous(labels = dollar_format()) +
+    labs(x = "Cost/km (Millions USD)",
+         y = "")
+
+transit_cost %>% 
+    filter(!is.na(cost_km_millions),
+           tunnel_per == 1,
+           country_name == "China") %>% 
+    mutate(city = fct_lump(city, 12)) %>% 
+    add_count(city) %>% 
+    mutate(city = glue::glue("{city} ({n})"),
+           city = fct_reorder(city, cost_km_millions)) %>% 
+    ggplot(aes(cost_km_millions, city)) +
+    geom_boxplot() +
+    scale_x_continuous(labels = dollar_format()) +
+    labs(x = "Cost/km (Millions USD)",
+         y = "") +
+    expand_limits(x = 0)
