103ed8ed910817ff7c2b50ddbfcdbfb1eca36880
Gaurav Sharma <36572215+grvsrm@users.noreply.github.com>
2021-01-27 12:00:03 +0530

    updated script

--- Scripts/2020-09-01_Crop-Yields_Model.R
+++ Scripts/2020-09-01_Crop-Yields_Model.R
@@ -23,3 +23,14 @@ top_countries <- land_use %>%
     slice_max(total_population_gapminder, n =30) %>% 
     pull(entity)
 
+
+key_crop_yields %>% 
+    clean_names() %>% 
+    pivot_longer(wheat_tonnes_per_hectare: bananas_tonnes_per_hectare,
+                 names_to = "crop",
+                 values_to = "yield",
+                 values_drop_na = T) %>% 
+    mutate(crop = str_remove(crop, "_tonnes_per_hectare")) %>% 
+    filter(crop %in% c("wheat", "maize", "barley", "rice"),
+           entity %in% top_countries)
+
